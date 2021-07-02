fcb2f33352883933af7b362d3454b810b4893b37
allanmosesfernandes <fernzallan14@gmail.com>
2021-07-02 12:49:53 +0530

    router-dom

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Directory from '../../components/directory'
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+Link
+} from "react-router-dom";
 
 
-function homepage() {
+function homepage(props) {
     return (
         <div className='homepage'>
-           <Directory />
+            {/* <Link to='/topic'>Topic</Link> */}
+            {/* <button onClick={() => props.history.push('/nutjob')}>Take me home</button> */}
+           <Directory history= {props.history}/>
         </div>
     )
 }
